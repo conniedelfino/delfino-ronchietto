20eb36e31036481d93b59118ae7648c8453c0b7f
conniedelfino <cdelfinochevallier@udesa.edu.ar>
2026-04-01 19:00:31 -0300

    components 1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@
 
 function App() {
   return (
-   <>hola</>
+   <div>
+    <h1>Hola React</h1>
+    <img src="https://definicion.de/wp-content/uploads/2009/12/paisaje-1.jpg" alt="imagen"/>
+   </div>
   );
 }
 
