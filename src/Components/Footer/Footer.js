9fb8b8154cc0ace204fc1d975379341988f2c532
conniedelfino <cdelfinochevallier@udesa.edu.ar>
2026-04-20 19:40:18 -0300

    clase del footer y correcion slicing de ipc en nowplaying

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 function Footer() {
   return (
-     <footer>
+     <footer className="alert alert-primary mt-4 text-center">
         <p>Marcos Ronchietto Meilan & Connie Delfino</p>
     </footer>
  );
